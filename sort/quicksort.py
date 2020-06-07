457e6d081f2fcef85cd29b35da85bbd16691efd7
TheQuantiX <thequantix12@gmail.com>
2020-06-07 20:07:54 +0300

    Now quicksort isn't lagging!

--- sort/quicksort.py
+++ sort/quicksort.py
@@ -1,16 +1,4 @@
-import random
-
-def insertion_sort(x):
-    i = 1
-    while i < len(x):
-        j = i
-        while j > 0 and x[j-1] > x[j]:
-            temp = x[j]
-            x[j] = x[j - 1]
-            x[j - 1] = temp
-            j = j - 1
-        i = i + 1
-    return x
+from insertion_sort import *
 
 def quicksort(x):
     #Extended Quicksort with Insertion sort when length is lower of equal than 7
@@ -19,18 +7,21 @@ def quicksort(x):
         return []
     if n == 1:
         return x
-    if n <= 7:
+    if n <= 16:
         return insertion_sort(x)
     lower = []
     higheq = []
-    r = random.randint(0, len(x) - 1)
-    pivot = x[r]
+    eq = []
+    pivot = insertion_sort([x[0], x[len(x) // 2], x[-1]])[1]
     for i in x:
-        if i >= pivot:
+        if i > pivot:
             higheq.append(i)
-        else:
+        elif i < pivot:
             lower.append(i)
+        else:
+            eq.append(i)
     newarr = []
     newarr.extend(quicksort(lower))
+    newarr.extend(eq)
     newarr.extend(quicksort(higheq))
     return newarr
